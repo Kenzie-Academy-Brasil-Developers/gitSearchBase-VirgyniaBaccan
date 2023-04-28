98a08f87e2a38c1060a48618dcde30d59eaccf52
Virgynia Baccan <virgyniabaccan@gmail.com>
2023-04-28 20:24:11 -0300

    correção do target no método OPEN() de : '_blank' para '_top'

--- src/scripts/error.js
+++ src/scripts/error.js
@@ -2,7 +2,7 @@ function returnHome() {
     const button = document.querySelector("button")
     button.addEventListener('click', () => {
         localStorage.clear()
-        open('http://127.0.0.1:5500/index.html', '_blank')
+        open('http://127.0.0.1:5500/index.html', '_top')
         
     })
 }
